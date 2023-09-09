7c97d63bae622de5923b1fa0f251ec4811f91502
blueskymanner <page111first@gmail.com>
2023-09-09 23:21:01 +0900

    added src/core dapps main

---
+++ src/main/offlineApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app class="walletBg">
+    <router-view />
+    <module-toast />
+    <module-global-modals />
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+  components: {
+    ModuleToast: () => import('@/modules/toast/ModuleToast.vue'),
+    ModuleGlobalModals: () =>
+      import('@/modules/global-modals/ModuleGlobalModals')
+  },
+  mounted() {
+    this.setOnlineStatus(false);
+    this.setOfflineApp(true);
+  },
+  methods: {
+    ...mapActions('wallet', ['setOfflineApp']),
+    ...mapActions('global', ['setOnlineStatus'])
+  }
+};
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/GlobalStyles.scss';
+@import '@myetherwallet/mew-components/src/assets/styles/global.scss';
+</style>
